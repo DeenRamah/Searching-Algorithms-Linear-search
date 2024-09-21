603d6b0c4b17ca71d39111b3fe97c7e9337facf2
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-09-21 23:36:31 +0300

    Create searcht.ts

---
+++ searcht.ts
@@ -0,0 +1,5 @@
+const fruits: string[]=["apple", "banana","mango","grape"];
+const targetFruit="mango";
+
+const fruitIndex = linearSearch(fruits, targetFruits);
+console.log(`fruit $(fruitIndex)`);
